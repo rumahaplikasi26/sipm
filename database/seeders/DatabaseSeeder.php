dc7d863af7fafc0f4475289bad145b444ec16520
Achmad Fatoni <2inotechno0@gmail.com>
2025-01-24 09:31:24 +0700

    Update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -42,6 +42,6 @@ class DatabaseSeeder extends Seeder
         $this->call(ShiftSeeder::class);
         // $this->call(ShiftScheduleSeeder::class);
 
-        $this->call(AttendanceSeeder::class);
+        // $this->call(AttendanceSeeder::class);
     }
 }
